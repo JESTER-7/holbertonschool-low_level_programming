b4e002f56cf60b00a92e9c42ad3f115dbaac3332
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-22 20:01:03 +0000

    m

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - ooo
  * Return: 0
  */
-int main(void){
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
