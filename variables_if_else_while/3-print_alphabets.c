258f231486ca6daa748ef6caebafd9c0b2e7ac9d
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-23 11:35:47 +0000

    m

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int i;
 	int j;
-
+	
 	for (i = 'a'; i <= 'z'; i++)
 	{
-	putchar(i);
+		putchar(i);
 	}
 	for (j = 'A'; j <= 'Z'; j++)
-        {
-        putchar(j);
-        }
+	{
+		putchar(j);
+	}
 	putchar('\n');
 return (0);
 }
