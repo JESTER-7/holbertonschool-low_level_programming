78dece33464d8a64924c4576e504a518992a3039
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-23 11:30:03 +0000

    m

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - ooo
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+return (0);
+}
