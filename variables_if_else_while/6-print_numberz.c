fcf898d20d6166680afdb1f875d9bd906766de44
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-23 11:47:39 +0000

    m

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - ooo
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+return (0);
+}
