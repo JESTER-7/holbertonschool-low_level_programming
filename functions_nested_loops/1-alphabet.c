aa24909376d5b3795c448feae6150c196fb4c1e9
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-23 12:24:22 +0000

    m

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_alphabet - ooo
+ * @i: i
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
