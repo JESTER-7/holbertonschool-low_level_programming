4222198afd7a061f006df21e132c7d559e2c873b
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-23 11:58:19 +0000

    m

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - ooo
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+return (0);
+}
