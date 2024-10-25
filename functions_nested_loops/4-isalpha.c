36f38862e548b038795330c432b62f2c5bfe42dc
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-25 13:25:31 +0000

    m

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - ooo
+ * @c: o
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
