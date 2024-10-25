c94f1da420eafa4b7839666b6fae907643604f45
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-25 13:46:22 +0000

    m

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - ooo
+ * @int: ooo
+ * Return: ooo
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
