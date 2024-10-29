2b19f0be6e348d5ca0202ee171873c76d49d48c8
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-29 10:14:09 +0000

    m

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,11 @@ void print_triangle(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	for (a = 0; a < size; a++)
+	for (a = 1; a <= size; a++)
 	{
-		for (b = 0; b <= a; b++)
+		for (b = 1; b <= size; b++)
 		{
-			if (b <= size - 1)
+			if (b <= size - a)
 				_putchar(' ');
 			else
 				_putchar('#');
