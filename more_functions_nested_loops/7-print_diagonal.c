868d2914fecfe54e369a76c988088fd4442b359b
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-29 09:03:53 +0000

    m

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - ooo
+ * @n: ooo
+ * Return: ooo
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
