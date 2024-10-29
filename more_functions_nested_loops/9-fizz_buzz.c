c916fae217211764ef4e2752efe8f7e030275261
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-29 09:48:42 +0000

    m

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,15 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 5 == 0 && a % 3 == 0)
-                {
-                        printf("FizzBuzz ");
-                }
+			printf("FizzBuzz ");
+		else if (a == 100)
+			printf("Buzz");
 		else if (a % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (a % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", a);
-		}
 	}
 	printf("\n");
 	return (0);
