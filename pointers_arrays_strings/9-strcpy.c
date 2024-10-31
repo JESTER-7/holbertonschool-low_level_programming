9916d4c45c9cef1595c2b56e3a909f88a9409038
JESTER-7 <arthur.urbano33@gmail.com>
2024-10-31 23:55:32 +0000

    m

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - ooo
+ * @dest: ooo
+ * @src: ooo
+ * Return: ooo
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	char *b;
+
+	while (src[a] != '\0')
+		a++;
+	b = src;
+	src = dest;
+	dest = b;
+	return (dest);
+}
