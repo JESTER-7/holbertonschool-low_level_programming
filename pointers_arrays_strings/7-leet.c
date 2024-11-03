4a6cb8a479ef1b22cab8dfddb74c795fa4c795c5
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-03 21:21:08 +0000

    m

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - ooo
+ * @s: ooo
+ * Return: ooo
+ */
+char *leet(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == 'a' || s[a] == 'A')
+			s[a] = '4';
+		else if (s[a] == 'e' || s[a] == 'E')
+                        s[a] = '3';
+		else if (s[a] == 'o' || s[a] == 'O')
+                        s[a] = '0';
+		else if (s[a] == 't' || s[a] == 'T')
+                        s[a] = '7';
+		else if (s[a] == 'l' || s[a] == 'L')
+                        s[a] = '1';
+	}
+	return (s);
+}
