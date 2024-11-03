c74919bcd02609b30f5ecb3b233de4b40da58164
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-03 22:49:34 +0000

    m

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,16 +8,15 @@ int _atoi(char *s)
 {
 	unsigned int nombre = 0;
 	int signe = 1;
-	
+
 	do {
 		if (*s == '-')
 			signe *= -1;
 
 		else if (*s >= '0' && *s <= '9')
-			nombre = (nombre * 10) + (*s - '0'); 
+			nombre = (nombre * 10) + (*s - '0');
 		/* passe le nombre en entier au lieu du char*/
-
-		else if (nombre > 0) 
+		else if (nombre > 0)
 			/* si c'est autre chose qu'un entier on recommence la boucle*/
 			break;
 
