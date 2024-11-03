5802bd788d55b49811a4bdc02540e2af7cff6173
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-03 22:13:28 +0000

    m

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - ooo
+ * @str: ooo
+ * Return: ooo
+ */
+char *cap_string(char *str)
+{
+	int a = 0;
+
+	if (str[a] >= 'a' && str[a] <= 'z')
+	{
+		str[a] -= 32;
+	}
+	char separators[] = " \t\n,;.!?\"(){}";
+	
+	for (a = 1; str[a] != '\0'; a++)
+	{
+		int b;
+
+		for (b = 0; separators[b] != '\0'; b++)
+		{
+			if (str[a - 1] == separators[b])
+			{
+				if (str[a] >= 'a' && str[a] <= 'z')
+				{
+					str[a] -= 32;
+				}
+				break;
+			}
+		}
+	}
+	return (str);
+}
