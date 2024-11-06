3054cef71e394ef8c3f01f1e616ffc51daf2c12e
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-06 11:00:17 +0000

    m

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s: the string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
