51c454e6383bff4c1562c6f173d9459530139374
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-07 13:50:03 +0100

    m

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,1 +1,16 @@
 #include "main.h"
+/**
+ * _pow_recursion - give the power of a number
+ * @x: the number
+ * @y: the power
+ * Return: the result
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+    return (-1);
+    else if (y == 0)
+    return (1);
+    else
+    return (x * _pow_recursion(x, y - 1));
+}
