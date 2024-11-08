8b1f750459f6b16420c7641b9f436957b2977309
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-08 21:53:08 +0000

    m

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,14 +8,13 @@
 void print_diagsums(int *a, int size)
 {
 	int i, diag_1, diag_2;
-	
+
 	diag_1 = 0;
 	diag_2 = 0;
-	
 	for (i = 0; i < size; i++)
-	{   
-		diag_1 += a[i*(size+1)];
-		diag_2 += a[(i+1)*(size-1)];
+	{
+		diag_1 += a[i * (size + 1)];
+		diag_2 += a[(i + 1) * (size - 1)];
 	}
 	printf("%d, ", diag_1);
 	printf("%d\n", diag_2);
