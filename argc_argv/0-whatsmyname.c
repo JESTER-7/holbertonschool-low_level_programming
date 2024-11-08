87d0061afc1efbe1619624d33666d96295875db7
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-08 01:43:39 +0000

    m

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
- *
+ * main - print the name of the exe
+ * @argc: the arguments
+ * @argv: array of string
+ * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < argc; i++)
-	       printf("%s", argv[i]);
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+			_putchar(argv[i][j]);
+	}
+	_putchar('\n');
 return (0);
 }
-
