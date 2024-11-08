3bcde4ba828bea9ec5d8621f3bf9029dce60fe97
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-08 19:07:17 +0100

    m

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,18 +1,26 @@
 #include "main.h"
 /**
- * is_prime_number - return 1 if the number is prime
- * @n: numbers
+ * increment - increment
+ * @a: variable to count and test all the numbers
+ * @n: the number
+ * Return: increment a+1
+ */
+int increment(int a, int n)
+{
+    if (a >= n) /* check if n can be divided, if not return 1 and if a = n too */
+        return (1);
+    if (n % a == 0) /* if n can be divided it's not prime */
+        return 0;
+    return increment(a+1, n);
+}
+/**
+ * is_prime_number - say if a number is prime or not
+ * @n: the number to test
  * Return: 1 or 0
  */
 int is_prime_number(int n)
 {
-    int i = 2;
-
-    if (n == 0)
+    if (n == 1 || n < 0)
         return (0);
-    is_prime_number(n - 1);
-    if (n % i == 0)
-        return (0);
-    else
-        return (1);
+    return increment(2,n);
 }
