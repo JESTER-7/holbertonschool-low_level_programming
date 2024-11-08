2d8d45b817a04ee0a53cbcd72f13db43a05c30ea
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-08 21:35:00 +0000

    m

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,30 +1,17 @@
 #include "main.h"
 /**
- * main - multiplie two numbers
+ * main - mutiply numbers
  * @argc: arguments
  * @argv: Array of * to arguments
- * Return: 0
+ * Return: 1 or 0
  */
 int main(int argc, char *argv[])
 {
-    int i, j, k, l;
-    char a;
-    
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 1; argv[i][j] != '\0'; j++)
-        {
-            
-        }
-        for (k = 1; argv[i][k] != '\0'; k++)
-        {
-            l += argv[i][k]
-        }
-    }
-    i = argv[1][0] - '0';
-    j = argv[2][0] - '0';
-    k = (i * j) + '0';
-    _putchar(k);
-    _putchar('\n');
-    return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
