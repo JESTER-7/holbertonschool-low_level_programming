e0dd93f939a738436d522d499530e57f532da13b
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-08 21:17:12 +0000

    m

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -7,20 +7,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int a, b;
+	int a, b;
 
-    if (*needle == '\0')
-        return (haystack);
-
-    for (a = 0; haystack[a] != '\0'; a++)
-    {
-        for (b = 0; needle[b] != '\0'; b++)
-        {
-            if (haystack[a + b] != needle[b])
-                break;
-        }
-            if (needle[b] == '\0')
-                return (haystack + a);
-    }
-    return (NULL);
+	if (*needle == '\0')
+		return (haystack);
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+			if (haystack[a + b] != needle[b])
+				break;
+		}
+		if (needle[b] == '\0')
+			return (haystack + a);
+	}
+	return (NULL);
 }
