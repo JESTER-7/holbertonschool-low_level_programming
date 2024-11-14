8d8f69b92c060c67347b605029553979407b268c
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-14 09:03:27 +0000

    m

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -6,19 +6,19 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    int a;
-    int b = 0;
-
-    if (str == NULL)
-        return (NULL);
-    while (str[b] != '\0')
-        b++;
-    duplicate = malloc(sizeof(char) * (b + 1)); /* +1 including the null operator */
-    if (duplicate == NULL)
-        return (NULL);
-    for (a = 0; a < b; a++)
-        duplicate[a] = str[a]; /* copy str to duplicate */
-    duplicate[b] = '\0'; /* mark the end of duplicate[b] */
-    return (duplicate);
+	char *duplicate;
+	int a;
+	int b = 0;
+	
+	if (str == NULL)
+		return (NULL);
+	while (str[b] != '\0')
+		b++;
+	duplicate = malloc(sizeof(char) * (b + 1)); /* +1 including the null operator */
+	if (duplicate == NULL)
+		return (NULL);
+	for (a = 0; a < b; a++)
+		duplicate[a] = str[a]; /* copy str to duplicate */
+	duplicate[b] = '\0'; /* mark the end of duplicate[b] */
+	return (duplicate);
 }
