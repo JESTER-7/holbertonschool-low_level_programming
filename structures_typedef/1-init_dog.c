ed2e749e68e1712bbee719bf278d9ff9078934f2
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-14 02:57:58 +0100

    m

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * 
+ * 
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d != NULL)
+    {
+        d->name = name;
+        d->owner = owner;
+        d->age = age;
+    }
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dog
+typedef struct dog
 {
     char *name;
     char *owner;
     float age;
-};
+} dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
