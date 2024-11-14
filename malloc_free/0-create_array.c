e988d36533442542ae95973e82bd5638ee9f27b7
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-14 09:01:52 +0000

    m

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,17 +7,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int a;
+	char *array;
+	unsigned int a;
 
-    if (size == 0)
-        return (NULL);
-    array = malloc(sizeof(char) * size);
-    if (array == NULL)
-        return (NULL);
-    for (a = 0; a < size; a++)
-    {
-        array[a] = c;
-    }
-    return (array);
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+	for (a = 0; a < size; a++)
+		array[a] = c;
+	return (array);
 }
