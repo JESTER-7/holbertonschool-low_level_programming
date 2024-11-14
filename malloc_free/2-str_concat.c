8667657e5d88be245ddc0b407c39498bf495fbdb
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-14 09:07:25 +0000

    m

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,26 +7,26 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *final_string;
-    int a = 0;
-    int b = 0;
-    int i, j;
+	char *final_string;
+	int a = 0;
+	int b = 0;
+	int i, j;
 
-    if (s1 == NULL)
-        return ("");
-    if (s2 == NULL)
-        return ("");
-    while (s1[a] != '\0')
-        a++;
-    while (s2[b] != '\0')
-        b++;
-    final_string = malloc(sizeof(char) * (a + b + 1));
-    if (final_string == NULL)
-        return (NULL);
-    for (i = 0; i < a; i++)
-        final_string[i] = s1[i];
-    for (j = 0; j < b; j++)
-        final_string[i + j] = s2[j];
-    final_string[i + j] = '\0';
-    return (final_string);
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
+		return ("");
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+	final_string = malloc(sizeof(char) * (a + b + 1));
+	if (final_string == NULL)
+		return (NULL);
+	for (i = 0; i < a; i++)
+		final_string[i] = s1[i];
+	for (j = 0; j < b; j++)
+		final_string[i + j] = s2[j];
+	final_string[i + j] = '\0';
+	return (final_string);
 }
