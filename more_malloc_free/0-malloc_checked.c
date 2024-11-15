65cc6f10dbb9f3687549f893eaca5df019dcfef8
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-15 22:20:13 +0000

    m

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,16 @@
 /**
  * malloc_checked - allocate memory
  * @b: number of bytes
+ * Return: p
  */
 void *malloc_checked(unsigned int b)
 {
-    void *p;
+	void *p;
 
-    p = malloc(b); /* allocate b bytes of memory */
-    if (p == NULL)
-    {
-        exit(98); /* end the process with the value 98 */
-    }
-    return (p);
+	p = malloc(b); /* allocate b bytes of memory */
+	if (p == NULL)
+	{
+		exit(98); /* end the process with the value 98 */
+	}
+	return (p);
 }
--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,16 +9,16 @@
  */
 int *array_range(int min, int max)
 {
-    int *array;
-    int a, size;
+	int *array;
+	int a, size;
 
-    if (min > max)
-        return (NULL);
-    size = max - min + 1;
-    array = malloc(sizeof(int) * size);
-    if (array == NULL)
-        return (NULL);
-    for (a = 0; a < size; a++)
-        array[a] = min + a;
-    return (array);
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	array = malloc(sizeof(int) * size);
+	if (array == NULL)
+		return (NULL);
+	for (a = 0; a < size; a++)
+		array[a] = min + a;
+	return (array);
 }
