80b7df5c4a97623bc27ade1d8045fabecd2adcb3
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-15 22:35:09 +0000

    m

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,32 +11,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    char *cpy_name;
-    char *cpy_owner;
+	dog_t *dog;
+	char *cpy_name;
+	char *cpy_owner;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return(NULL);
-    cpy_name = malloc(_strlen(name) + 1);
-    if (cpy_name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
-    _strcpy(cpy_name, name);
-    cpy_owner = malloc(_strlen(owner) + 1);
-    if (cpy_owner == NULL)
-    {
-        free(cpy_name);
-        free(dog);
-        return (NULL);
-    }
-    _strcpy(cpy_name, name);
-    dog->name = cpy_name;
-    dog->age = age;
-    dog->owner = cpy_owner;
-    return (dog);
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return(NULL);
+	cpy_name = malloc(_strlen(name) + 1);
+	if (cpy_name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	_strcpy(cpy_name, name);
+	cpy_owner = malloc(_strlen(owner) + 1);
+	if (cpy_owner == NULL)
+	{
+		free(cpy_name);
+		free(dog);
+		return (NULL);
+	}
+	_strcpy(cpy_name, name);
+	dog->name = cpy_name;
+	dog->age = age;
+	dog->owner = cpy_owner;
+	return (dog);
 }
