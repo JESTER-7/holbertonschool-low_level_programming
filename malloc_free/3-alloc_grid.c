ad39ebccc23474a630b03657294666339bf5b432
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-15 19:32:31 +0100

    m

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,24 +12,19 @@ int **alloc_grid(int width, int height)
 
     if (width <= 0 || height <= 0)
         return (NULL);
-    /* Allocate memory for the array of row pointers */
     array = malloc(sizeof(int *) * height);
     if (array == NULL)
         return (NULL);
-
-    /* Allocate memory for each row and initialize elements to 0 */
     for (a = 0; a < height; a++)
     {
         array[a] = malloc(sizeof(int) * width);
         if (array[a] == NULL)
         {
-            /* Free previously allocated rows on failure */
             for (b = 0; b < a; b++)
                 free(array[b]);
             free(array);
             return (NULL);
         }
-        /* Initialize elements of the row to 0 */
         for (b = 0; b < width; b++)
             array[a][b] = 0;
     }
