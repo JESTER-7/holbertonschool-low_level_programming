d9c785e30631d04ba797af30c952d4055fae32aa
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-21 08:19:56 +0000

    m

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
 /**
- * 
+ * print_name - print the name of the guy
+ * @name: his name
+ * @f: the function
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
-    f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
