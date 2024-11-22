c6253041d4a81667ea8dbfbe148df0796786b665
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-22 18:15:49 +0100

    m

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+/**
+ * op_add - add 2 num
+ * @a: 1st
+ * @b: 2nd
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - difference between 2 num
+ * @a: 1st
+ * @b: 2nd
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply 2 num
+ * @a: 1st
+ * @b: 2nd
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide 2 num
+ * @a: 1st
+ * @b: 2nd
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - give the modulo of 2 num
+ * @a: 1st
+ * @b: 2nd
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
