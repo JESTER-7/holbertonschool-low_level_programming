a5f3282ebfb0c6cc75efea723a8eab63a0f769c3
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-22 13:20:47 +0100

    m

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,16 +6,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list s;
+	va_list s;
 
-    int sum = 0;
-    unsigned int a;
+	int sum = 0;
+	unsigned int a;
 
-    if (n == 0)
-        return (0);
-    va_start (s, n);
-    for (a = 0; a < n; a++)
-        sum += va_arg (s, int);
-    va_end (s);
-    return (sum);
+	if (n == 0)
+		return (0);
+	va_start (s, n);
+	for (a = 0; a < n; a++)
+		sum += va_arg (s, int);
+	va_end (s);
+	return (sum);
 }
