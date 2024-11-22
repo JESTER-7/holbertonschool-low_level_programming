e776336992a8d0dcc90a9d4d2f410a3decde2924
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-22 13:19:36 +0100

    m

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,12 +2,15 @@
 /**
  * sum_them_all - sum all the int
  * @n: all the int
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
     va_list s;
+
     int sum = 0;
     unsigned int a;
+
     if (n == 0)
         return (0);
     va_start (s, n);
