ca67a8648d0a20df08328301f430750fa50015c0
JESTER-7 <arthur.urbano33@gmail.com>
2024-12-08 22:33:48 +0100

    append

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,32 +1,32 @@
 #include "main.h"
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: The name of the file.
- * @text_content: The NULL-terminated string to add at the end of the file.
- * Return: 1 on success, -1 on failure.
+ * append_text_to_file - add text at the end of a file
+ * @filename: filename
+ * @text_content: string to add at the end of the file
+ * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    size_t length = 0;
+	int i;
+	ssize_t b_write;
+	size_t len = 0;
 
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        while (text_content[length])
-            length++;
-        bytes_written = write(fd, text_content, length);
-        if (bytes_written == -1 || (size_t)bytes_written != length)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-    close(fd);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	i = open(filename, O_WRONLY | O_APPEND);
+	if (i == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		b_write = write(i, text_content, len);
+		if (b_write == -1 || (size_t)b_write != len)
+		{
+			close(i);
+			return (-1);
+		}
+	}
+	close(i);
+	return (1);
 }
