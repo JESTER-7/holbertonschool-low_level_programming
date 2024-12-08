2fb862c8f66b11710d455e2df4cd26cb9b8e6c88
JESTER-7 <arthur.urbano33@gmail.com>
2024-12-08 22:21:14 +0100

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,43 +1,42 @@
 #include "main.h"
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- * Return: The actual number of letters it could read and print,
- *         or 0 if the file cannot be opened or read, or if `write` fails.
+ * read_textfile - read a file
+ * @filename: filename
+ * @letters: number of letters
+ * Return: number of letters
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int i;
+    ssize_t b_read, b_write;
+	char *a;
 
-    if (filename == NULL)
-        return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-    free(buffer);
-    close(fd);
-    return (bytes_written);
+	if (filename == NULL)
+		return (0);
+	i = open(filename, O_RDONLY);
+	if (i == -1)
+		return (0);
+	a = malloc(sizeof(char) * letters);
+	if (a == NULL)
+	{
+		close(i);
+		return (0);
+	}
+	b_read = read(i, a, letters);
+	if (b_read == -1)
+	{
+		free(a);
+		close(i);
+		return (0);
+	}
+	b_write = write(STDOUT_FILENO, a, b_read);
+	if (b_write == -1 || b_write != b_read)
+	{
+		free(a);
+		close(i);
+		return (0);
+	}
+	free(a);
+	close(i);
+	return (b_write);
 }
